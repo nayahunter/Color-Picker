9a56af68d10b4370ddc85d01549bf967af77f875
nayahunter <nhunter@millerschool.org>
2019-04-25 11:26:18 -0400

    first commit

---
+++ colorpicker.js
@@ -0,0 +1,55 @@
+var bodyColor= document.getElementById('bodyColor');
+var purpleBox= document.getElementById('purple');
+
+var colorPickerPurple= function(){
+  bodyColor.style.backgroundColor = "purple";
+}
+
+purpleBox.addEventListener("click", colorPickerPurple);
+
+
+var bodyColor= document.getElementById('bodyColor');
+var redBox= document.getElementById('red');
+
+var colorPickerRed= function(){
+  bodyColor.style.backgroundColor = "red";
+}
+
+redBox.addEventListener("click", colorPickerRed);
+
+
+var bodyColor= document.getElementById('bodyColor');
+var blueBox= document.getElementById('blue');
+
+var colorPickerBlue= function(){
+  bodyColor.style.backgroundColor = "blue";
+}
+
+blueBox.addEventListener("click", colorPickerBlue);
+
+var bodyColor= document.getElementById('bodyColor');
+var greenBox= document.getElementById('green');
+
+var colorPickerGreen= function(){
+  bodyColor.style.backgroundColor = "green";
+}
+
+greenBox.addEventListener("click", colorPickerGreen);
+
+var bodyColor= document.getElementById('bodyColor');
+var pinkBox= document.getElementById('pink');
+
+var colorPickerPink= function(){
+  bodyColor.style.backgroundColor = "pink";
+}
+
+pinkBox.addEventListener("click", colorPickerPink);
+
+var bodyColor= document.getElementById('bodyColor');
+var yellowBox= document.getElementById('yellow');
+
+var colorPickerYellow= function(){
+  bodyColor.style.backgroundColor = "yellow";
+}
+
+yellowBox.addEventListener("click", colorPickerYellow);
